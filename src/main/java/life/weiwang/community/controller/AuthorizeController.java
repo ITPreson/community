d32f72f48a9da414f9a1be1b5427b9711c6fd036
weiwang <15735659382@163.com>
2019-09-26 07:30:58 +0800

    add h2 database,add check cookies

--- src/main/java/life/weiwang/community/controller/AuthorizeController.java
+++ src/main/java/life/weiwang/community/controller/AuthorizeController.java
@@ -2,34 +2,73 @@ package life.weiwang.community.controller;
 
 import life.weiwang.community.dto.AccessTokenDTO;
 import life.weiwang.community.dto.GithubUser;
+import life.weiwang.community.mapper.UserMapper;
+import life.weiwang.community.model.User;
 import life.weiwang.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     GitHubProvider gitHubProvider;
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.redirect.url}")
+    private String redirect_url;
 
+    @Autowired
+    UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name = "code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("eb7d8d8fa230ed6e1a12");
-        accessTokenDTO.setClient_secret("030b6d6de83e4860aaa1388f8818b1b9c815e35e");
-        accessTokenDTO.setRedirect_uri("http://localhost:8886/callback");
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
+        accessTokenDTO.setRedirect_url(redirect_url);
         accessTokenDTO.setCode(code);
 
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        GithubUser githubUser = gitHubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setAccount_id(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //写入cookie
+            response.addCookie(new Cookie("token",token));
 
-        return "index";
+            /*初始的做法：
+                //登录成功，写入session，自动分配cookie
+                request.getSession().setAttribute("githubUser",githubUser);*/
+
+            return "redirect:/";
+
+        }else {
+            //登录失败    redirect前缀：重定向。后面要跟全路径
+            return "redirect:/";
+        }
+
+
     }
 
 }
---
+++ src/main/java/life/weiwang/community/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package life.weiwang.community.mapper;
+
+import life.weiwang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into user(name,account_id,token,gmt_Create,gmt_Modified) values(#{name},#{account_id},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
