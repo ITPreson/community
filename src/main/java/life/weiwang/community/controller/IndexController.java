5e239af25189cc8f87d99614045ed9b6e3f29b2c
weiwang <15735659382@163.com>
2019-09-28 13:16:08 +0800

    add vaild interception

--- src/main/java/life/weiwang/community/controller/IndexController.java
+++ src/main/java/life/weiwang/community/controller/IndexController.java
@@ -1,19 +1,13 @@
 package life.weiwang.community.controller;
 
 import life.weiwang.community.dto.PaginationDTO;
-import life.weiwang.community.dto.QuestionDTO;
 import life.weiwang.community.mapper.UserMapper;
-import life.weiwang.community.model.User;
 import life.weiwang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -24,24 +18,10 @@ public class IndexController {
     QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "3") Integer size) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
 
-                }
-
-            }
         /**
          * 这里需要查询的QuestionDTO是封装了question和user模型的模型，所以需要一个中间
          * service把这两个查询包装起来
--- src/main/java/life/weiwang/community/controller/ProfileController.java
+++ src/main/java/life/weiwang/community/controller/ProfileController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -36,23 +35,7 @@ public class ProfileController {
             model.addAttribute("sectionName", "最新回复");
         }
 
-        Cookie[] cookies = request.getCookies();
-        User user = null;
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-
-                }
-
-            }
-        }
-
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             return "redirect:/";
         }
